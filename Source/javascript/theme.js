0fba1a646fe40af14f9b63fc2ea5218d521622aa
Mikołaj Sobczak <teczaksupermiki@gmail.com>
2024-12-01 17:31:20 +0100

    only workshow left ;-;

--- Source/javascript/theme.js
+++ Source/javascript/theme.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
 
-    document.addEventListener('DOMContentLoaded', ()=> {
-        const themeLink = document.getElementById("theme-style-file");
-        const savedTheme = localStorage.getItem("theme") || "light-theme.css";
-        themeLink.setAttribute("href", savedTheme);
-    })
-
     $("#ToggleBtn").on("click", () => {
         const themeLink = document.getElementById("theme-style-file");
         const currentTheme = themeLink.getAttribute("href");
         if (currentTheme === "Source/CSS/themes/light-main-page.css") {
             newTheme = "Source/CSS/themes/dark-main-page.css";
             $(".slider").addClass("dark-slider");
+            changeProfilesImages(true);
         }else {
             newTheme = "Source/CSS/themes/light-main-page.css";
             $(".slider").removeClass("dark-slider");
+            changeProfilesImages(false);
         }
         themeLink.setAttribute("href", newTheme);
-        localStorage.setItem("theme", newTheme);
 
     });
 });
+function changeProfilesImages(isDark){
+    facebook = $('#fbImg');
+    instagram = $('#igImg');
+    github = $('#ghImg');
+    if(isDark){
+        facebook.attr('src', "Source/social-media/fb/Facebook_Logo_Primary.png");
+        instagram.attr('src', "Source/social-media/ig/Instagram_Glyph_Gradient.png");
+        github.attr('src', "Source/social-media/github/github-mark.png");
+    }
+    else{
+        facebook.attr('src', "Source/social-media/fb/Facebook_Logo_Secondary.png");
+        instagram.attr('src', "Source/social-media/ig/Instagram_Glyph_White.png");
+        github.attr('src', "Source/social-media/github/github-mark-white.png");
+    }
+}
