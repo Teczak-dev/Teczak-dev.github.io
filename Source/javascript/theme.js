ad916272227e673d8a7875fb325e9bbe6ed6febe
Mikołaj Sobczak <teczaksupermiki@gmail.com>
2024-12-01 13:07:51 +0100

    1/5 of website done :>

--- Source/javascript/theme.js
+++ Source/javascript/theme.js
@@ -1,8 +1,23 @@
-$("#ToggleBtn").on("click",()=>{
-    if ( $("#ToggleBtn").prop('checked') ) {
-        // tryb ciemny
-    }
-    else{
-        // tryb jasny
-    }
-})
+$(document).ready(function() {
+
+    document.addEventListener('DOMContentLoaded', ()=> {
+        const themeLink = document.getElementById("theme-style-file");
+        const savedTheme = localStorage.getItem("theme") || "light-theme.css";
+        themeLink.setAttribute("href", savedTheme);
+    })
+
+    $("#ToggleBtn").on("click", () => {
+        const themeLink = document.getElementById("theme-style-file");
+        const currentTheme = themeLink.getAttribute("href");
+        if (currentTheme === "Source/CSS/themes/light-main-page.css") {
+            newTheme = "Source/CSS/themes/dark-main-page.css";
+            $(".slider").addClass("dark-slider");
+        }else {
+            newTheme = "Source/CSS/themes/light-main-page.css";
+            $(".slider").removeClass("dark-slider");
+        }
+        themeLink.setAttribute("href", newTheme);
+        localStorage.setItem("theme", newTheme);
+
+    });
+});
